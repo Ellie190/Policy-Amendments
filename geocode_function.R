1b53ff9b26a2323ee0a0cb1901881775c8c7f305
Eli Nimy <nimyellie0512@gmail.com>
2021-10-02 23:44:06 +0200

    Geocode function for Twitter Analysis

---
+++ geocode_function.R
@@ -0,0 +1,28 @@
+library(tidygeocoder)
+library(dplyr)
+
+city_input <- "london, uk"
+geo_loc <- tidygeocoder::geo(city_input, method = 'osm', lat = latitude, long = longitude) 
+
+# Access latitude 
+geo_loc$latitude[1]
+
+# Access longitude 
+geo_loc$longitude[1]
+
+## Miles 
+miles <- 100
+
+## Putting the latitude and longitude and miles 
+paste0(paste0(paste0(geo_loc$latitude[1], ","), paste0(geo_loc$longitude[1], ",")),
+       paste0(miles, "mi"))
+
+geocode_EK <- function(loc, mil) {
+  geo_loc <- tidygeocoder::geo(loc, method = 'osm', lat = latitude, long = longitude)
+  paste0(paste0(paste0(geo_loc$latitude[1], ","), paste0(geo_loc$longitude[1], ",")),
+         paste0(mil, "mi"))
+  
+}
+
+geocode_EK("london, uk", 100)
+
